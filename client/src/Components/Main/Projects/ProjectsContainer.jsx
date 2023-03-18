cc464b5eb4e21faeae70163ddf8e1ff3b05806ac
Re Julian <julianre2021@gmail.com>
2023-03-18 08:13:34 -0300

    Project Container

--- client/src/Components/Main/Projects/ProjectsContainer.jsx
+++ client/src/Components/Main/Projects/ProjectsContainer.jsx
@@ -1,25 +1,15 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
 import Project from './Project'
 import './Project.css'
 
 const ProjectsContainer = () => {
 
-  const [project, setProject] = useState([])
-
-  useEffect(()=>{
-    axios.get('http://localhost:4000/').then(data=>setProject(data.data))
-  },[])
-
   return (
     <section id='projects'>
         <h2 className='titles' data-aos="zoom-in">Projects</h2>
         <div>
-            {project.map((p,key)=>{
-              return(
-                <Project img={p.img} desc={p.description} link={p.link} key={key}/>
-              )
-            })}
+          <Project img={'https://res.cloudinary.com/tuko/image/upload/v1678645206/portfolio/lamatita_ey01hy.png'} desc={'Project for a bookshop. Allows you to create and edit articles and contact by WhatsApp.'}/>
+          <Project img={'https://res.cloudinary.com/tuko/image/upload/v1678645314/portfolio/importados_ldbyo5.png'} desc={'Project carried out for a venture dedicated to selling perfumes. It allows you to add, edit and delete products and contact by whatsapp.'}/>
+          <Project img={'https://res.cloudinary.com/tuko/image/upload/v1664367426/portfolio/SkateShop_osd7cc.png'} desc={'This was a final test for a course I did in CoderHouse. Its an eCommerce created with React and Firebase.'} link={'https://skateshop-seven.vercel.app/'}/>
         </div>
     </section>
   )
